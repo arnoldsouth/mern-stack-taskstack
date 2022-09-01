7b332ee20be1daccdb4002336b36f2f160f22b79
arnoldsouth <arnoldsouth@gmail.com>
2022-09-01 16:41:01 -0500

    Pushing previous revert

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5005;
 
 connectDB();
 
@@ -13,13 +13,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Use routes
 app.use('/api/tasks', require('./routes/taskRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 
-// Serve frontend static assets if in production
+// Serve frontend
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
   app.get('*', (req, res) =>
